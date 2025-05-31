6b29c860fdcf3bc021b5bab16c089cc6f60737c4
Emre Er <emre@Mac.home>
2025-05-31 17:00:29 +0300

    SEO ve GA meta etiketleri

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,6 +22,27 @@ export default function Document() {
             `,
           }}
         />
+        {/* SEO Temel Meta Etiketleri */}
+        <meta name="description" content="Yapay zekâ destekli Türkçe hukuk bilgilendirme platformu. Kiracı hakları, işten çıkarma, trafik cezaları ve daha fazlası için ücretsiz destek alın." />
+        <meta name="keywords" content="hukuk, kiracı hakları, işten çıkarma, trafik cezası, avukat, danışmanlık, Türkiye hukuku, yapay zeka hukuk" />
+        <meta name="author" content="Hukukirehber" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Sosyal Medya Önizleme (Open Graph) */}
+        <meta property="og:title" content="Hukukirehber" />
+        <meta property="og:description" content="Türkçe yapay zekâ destekli hukuki bilgilendirme asistanı. Ücretsiz ve anonim destek alın." />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:url" content="https://hukukirehber.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Kartı */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Hukukirehber" />
+        <meta name="twitter:description" content="Türkçe yapay zekâ destekli hukuki bilgilendirme asistanı." />
+        <meta name="twitter:image" content="/og-image.jpg" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
         <Main />
