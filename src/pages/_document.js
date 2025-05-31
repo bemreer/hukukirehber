0854dfef8c6a502df8a8e6d8551d898be52a4f3f
Emre Er <emre@Mac.home>
2025-05-31 16:08:18 +0300

    GA kodu next/script ile optimize edildi

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="tr">
       <Head>
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HM5M8E4M8E"></script>
-        <script
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HM5M8E4M8E"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
